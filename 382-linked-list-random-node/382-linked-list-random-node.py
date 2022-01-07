2c8bec32f59377cfdf733f3bd842751c2c94ef70
Rohan Raj <42442507+rohan1907@users.noreply.github.com>
2022-01-07 22:30:54 +0530

    Time: 146 ms (29.18%), Space: 17.3 MB (56.84%) - LeetHub

---
+++ 382-linked-list-random-node/382-linked-list-random-node.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+
+    def __init__(self, head: Optional[ListNode]):
+        self.head = head
+        
+
+    def getRandom(self) -> int:
+        scope = 1
+        chosen_value = 0
+        curr = self.head
+        
+        while curr:
+            if random.random()<1/scope:
+                chosen_value = curr.val
+            curr = curr.next
+            scope += 1
+        return chosen_value
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(head)
+# param_1 = obj.getRandom()
