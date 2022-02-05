417c07d7f5bcf0d05ccfac6dbaa3438b046510a2
Rohan Raj <42442507+rohan1907@users.noreply.github.com>
2022-02-05 23:37:03 +0530

    Time: 1 ms (100.00%), Space: 44 MB (57.10%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -9,32 +9,47 @@
  * }
  */
 
-class compare implements Comparator<ListNode>{
-    @Override
-    public int compare(ListNode l1, ListNode l2){
-        return l1.val - l2.val;
-    }
-}
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists == null) return null;
-        PriorityQueue<ListNode> pq = new PriorityQueue<>(new compare());
-        
-        for(ListNode li : lists){
-            ListNode head = li;
-            while(head!=null){
-                pq.offer(head);
-                head = head.next;
+          return partition(lists, 0, lists.length-1);
+    }
+    
+    public ListNode partition(ListNode[] lists, int i, int j){
+        while(i==j){
+            return lists[i];
+        }
+        if(i<j){
+            int mid = i + (j-i)/2;
+            ListNode l1 = partition(lists, i, mid);
+            ListNode l2 = partition(lists, mid+1, j);
+            
+            return sort(l1, l2);
+        }
+        else {
+            return null;
+        }        
+    }
+    
+    public ListNode sort(ListNode l1, ListNode l2){
+        ListNode head= new ListNode(0);
+        ListNode dummy = head;
+        while(l1!=null && l2!=null){
+            if(l1.val<=l2.val){
+                dummy.next = l1;
+                l1 = l1.next;
             }
+            else if(l2.val<l1.val){
+                dummy.next = l2;
+                l2 = l2.next;
+            }
+            dummy = dummy.next;
         }
-        if(pq.size() == 0) return null;
-        ListNode ans = pq.poll(), head = ans;
-        while(!pq.isEmpty()){
-            ans.next = pq.poll();
-            //System.out.print(ans.val+" ");
-            ans = ans.next;
+        if(l1!=null){
+            dummy.next = l1;
         }
-        if(ans.next!=null) ans.next = null;
-        return head;
+        if(l2!=null){
+            dummy.next = l2;
+        }
+        return head.next;
     }
 }
