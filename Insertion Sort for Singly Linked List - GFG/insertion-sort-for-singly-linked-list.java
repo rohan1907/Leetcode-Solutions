5921bd8e9feeb7c9220adc5ff51a88ada602c391
Rohan Raj <42442507+rohan1907@users.noreply.github.com>
2022-02-23 20:54:01 +0530

    Added solution - LeetHub

---
+++ Insertion Sort for Singly Linked List - GFG/insertion-sort-for-singly-linked-list.java
@@ -0,0 +1,100 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+class insertion
+{
+    Node head;  
+    Node tail;
+	public void addToTheLast(Node node) 
+	{
+	  if (head == null) 
+	  {
+	   head = node;
+	   tail = node;
+	  } 
+	  else 
+	  {
+	   tail.next = node;
+	   tail = node;
+	  }
+	}
+      void printList(Node head)
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	/* Drier program to test above functions */
+	public static void main(String args[])
+    {
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 while(t>0)
+         {
+			int n = sc.nextInt();
+			insertion llist = new insertion(); 
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+				llist.addToTheLast(new Node(a));
+			}
+			
+        Solution ob = new Solution();
+		head = ob.insertionSort(llist.head);
+		llist.printList(head);
+		
+        t--;		
+        }
+    }}// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+    */
+class Solution
+{
+    public static Node insertionSort(Node head_ref)
+    {
+        //code here
+        
+         Node head= new Node(Integer.MIN_VALUE);
+       head.next=head_ref;
+       Node cur=head_ref,pre=head;
+   
+       while(cur!=null){
+           if(cur.next!=null&&cur.next.data<cur.data){
+             while(pre.next!=null&&pre.next.data<cur.next.data){
+                 pre=pre.next;
+             }
+             Node temp=pre.next;
+            pre.next=cur.next;
+            cur.next=cur.next.next;
+            pre.next.next=temp;
+            pre=head;
+           }
+           else
+              cur=cur.next;
+       }
+          return head.next;
+    }
+}
