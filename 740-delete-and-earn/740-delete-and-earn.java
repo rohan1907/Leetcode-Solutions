60baa5da4b786c3688343e98e0ef6a9c66808f66
Rohan Raj <42442507+rohan1907@users.noreply.github.com>
2022-03-10 00:09:02 +0530

    Time: 6 ms (51.38%), Space: 44.8 MB (41.37%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.java
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -1,28 +1,24 @@
 class Solution {
     public int deleteAndEarn(int[] nums) {
+        int maxNumber = 0;
+        HashMap<Integer, Integer> points = new HashMap<>();
         
-        HashMap<Integer, Integer> mp = new HashMap<>();
-        for(int ele: nums){
-            mp.put(ele,mp.getOrDefault(ele,0)+1);
+        // Precompute how many points we gain from taking an element
+        for (int num : nums) {
+            points.put(num, points.getOrDefault(num, 0) + num);
+            maxNumber = Math.max(maxNumber, num);
         }
         
-        List<Integer> st = new ArrayList<Integer>(mp.keySet());
-        Collections.sort(st);
-        int earn1 = 0, earn2 = 0;
-        for(int i=0; i<st.size();i++){
-            int ele = st.get(i);
-            int currEarn = ele*mp.get(ele);
-            if (i>0 && st.get(i) == st.get(i-1)+1){
-                int temp = earn2;
-                earn2 = Math.max(currEarn+earn1, earn2);
-                earn1 = temp;
-            }
-            else{
-                int tmp = earn2;
-                earn2 = earn2+currEarn;
-                earn1 = tmp;
-            }
+        // Base cases
+        int twoBack = 0;
+        int oneBack = points.getOrDefault(1, 0);
+        
+        for (int num = 2; num <= maxNumber; num++) {
+            int temp = oneBack;
+            oneBack = Math.max(oneBack, twoBack + points.getOrDefault(num, 0));
+            twoBack = temp;
         }
-        return earn2;
+        
+        return oneBack;
     }
 }
