f0f457f4e4ddc4143793ec774fb62e4d98eff943
Rohan Raj <42442507+rohan1907@users.noreply.github.com>
2022-03-24 23:39:36 +0530

    Added solution - LeetHub

--- Form a palindrome - GFG/form-a-palindrome.java
+++ Form a palindrome - GFG/form-a-palindrome.java
@@ -1,47 +1,47 @@
 // { Driver Code Starts
-import java.io.*;
-import java.util.*; 
+//Initial Template for Java
 
-class GFG{
-    public static void main(String args[]) throws IOException { 
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(read.readLine());
-        
-        while(t-- > 0){
-            String str = read.readLine();
-
-            Solution ob = new Solution();
+        while(t-- > 0)
+        {
             
-            System.out.println(ob.countMin(str));
+            String S = read.readLine().trim();
+            Solution ob = new Solution();
+            System.out.println(ob.findMinInsertions(S));
         }
-    } 
-} // } Driver Code Ends
+    }
+}// } Driver Code Ends
 
 
 //User function Template for Java
 
 class Solution{
-    static int countMin(String str)
-    {
-        // code here
-        int n = str.length();
-       StringBuilder sb = new StringBuilder(str);
-       String t = sb.reverse().toString();
-       return n - lcs(str,t,n,n);
-        
-    }
-    static int lcs(String a ,String b,int m,int n){
-       int[][]dp = new int[m+1][n+1];
+   int[][] arr;
+   int findMinInsertions(String S){
+       // code here
+       arr = new int[S.length()][S.length()];
+       String S2 = new StringBuilder(S).reverse().toString();
        
-       for(int i=1;i<dp.length;i++){
-           for(int j=1;j<dp[0].length;j++){
-               if(a.charAt(i-1) == b.charAt(j-1)){
-                   dp[i][j] = dp[i-1][j-1] + 1;
-               }else{
-                   dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
-               }
-           }
+       return S.length()-lcs(S, S2, S.length()-1, S2.length()-1);
+   }
+   
+   int lcs(String s1, String s2, int i, int j)
+   {
+       if(i<0 || j<0) return 0;
+       if(arr[i][j] != 0) return arr[i][j];
+       if(s1.charAt(i)==s2.charAt(j)) 
+       {
+           return arr[i][j] = (1 + lcs(s1, s2, i-1, j-1));
        }
-       return dp[m][n];
+       
+       return arr[i][j] = Math.max(lcs(s1, s2, i-1, j), lcs(s1, s2, i, j-1));
    }
 }
