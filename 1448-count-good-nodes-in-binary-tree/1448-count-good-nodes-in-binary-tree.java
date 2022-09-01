8aa6c4a913754d58572b5a50116113bd71bc72d0
Rohan Raj <rajrohan192@gmail.com>
2022-09-01 23:31:26 +0530

    Time: 4 ms (40.75%), Space: 59.7 MB (56.67%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -13,25 +13,27 @@
  *     }
  * }
  */
-class Ans{
-    int a=0;
+class Ans {
+    int a = 0;
 }
+
 class Solution {
+
     public int goodNodes(TreeNode root) {
         Ans ans = new Ans();
-        helper(root,ans,root.val);
+        helper(root, ans, root.val);
         return ans.a;
     }
-    
-    public void helper(TreeNode root, Ans ans, int max){
-        if(root == null) return;
-        if(root.left == null && root.right == null){
-            if(root.val >= max) ans.a++;
+
+    public void helper(TreeNode root, Ans ans, int max) {
+        if (root == null) return;
+        if (root.left == null && root.right == null) {
+            if (root.val >= max) ans.a++;
             return;
         }
-        if(root.val >= max) ans.a++;
-        max = max< root.val? root.val:max;
-        helper(root.left,ans,max);
-        helper(root.right, ans,max);
+        if (root.val >= max) ans.a++;
+        max = max < root.val ? root.val : max;
+        helper(root.left, ans, max);
+        helper(root.right, ans, max);
     }
 }
